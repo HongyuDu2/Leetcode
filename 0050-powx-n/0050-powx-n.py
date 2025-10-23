de8c1993d6fb0ded843266013ce0a0005b0090a8
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2025-10-23 16:23:01 -0700

    Time: 0 ms (100%), Space: 18 MB (31.66%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,12 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def fast_pow(x, n):
+            if n == 0:
+                return 1
+            half = fast_pow(x, n // 2)
+            if n % 2 == 0:
+                return half * half
+            else:
+                return half * half * x
+
+        return fast_pow(x, n) if n >= 0 else 1 / fast_pow(x, -n)
