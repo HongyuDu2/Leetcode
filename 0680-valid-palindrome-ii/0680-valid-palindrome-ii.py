c123ce9777622a64870e090c2ff9c9d2f27a153f
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2025-10-24 14:13:38 -0700

    Time: 67 ms (32.43%), Space: 17.7 MB (91.61%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,70 +1,18 @@
-'''
-class Solution(object):
-    def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s_input = list(s)
-        s_input2 = s_input[::-1]         # 反转复制的列表
-
-        t = s_input
-        for i in range(len(s_input)):
-            if s_input[i] == s_input2[i]:
-                continue
-            else:
-                if i < len(s_input) - 1 and s_input[i] == s_input2[i + 1]:
-                    if  s_input[i + 1] != s_input2[i]:
-                        s_input2.pop(i)
-                        t = s_input2
-                        break
-                    else:
-                        s_input2.pop(i)
-                        t = s_input2
-                        break
-                elif i < len(s_input) - 1 and s_input[i + 1] == s_input2[i]:
-                    if s_input2[i + 1] != s_input[i]:
-                        s_input.pop(i)
-                        t = s_input
-                        break
-                    else:
-                        s_input2.pop(i)
-                        t = s_input2
-                        break
-                else:
-                    return False  # 如果没有符合条件的情况
-        
-        t_reverse = t[::-1]  # Create a reversed copy of t
-        for i in range(len(t)):
-            if t[i] == t_reverse[i]:  # Compare with the reversed copy
-                continue
-            else:
-                return False  # If any characters do not match, return False
-        
-        return True
-'''
-class Solution(object):
-    def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        def is_palindrome_range(left, right):
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def inner_test(left, right):
             while left < right:
                 if s[left] != s[right]:
                     return False
                 left += 1
                 right -= 1
             return True
-
-        left, right = 0, len(s) - 1
+        
+        left, right = 0, len(s)-1
 
         while left < right:
             if s[left] != s[right]:
-                # Check if we can skip either character
-                return is_palindrome_range(left + 1, right) or is_palindrome_range(left, right - 1)
+                return inner_test(left+1, right) or inner_test(left, right-1)
             left += 1
             right -= 1
-
-        return True  # If the loop completes, it's a palindrome
-                
+        return True
