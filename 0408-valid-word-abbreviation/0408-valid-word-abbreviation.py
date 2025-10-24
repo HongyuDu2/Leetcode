3f59e558632580317f11ce8f58c02ba48f10e209
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2025-10-24 13:48:03 -0700

    Time: 0 ms (100%), Space: 18 MB (16.95%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -1,52 +1,19 @@
-class Solution(object):
-    def validWordAbbreviation(self, word, abbr):
-        
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         i = 0
-        Count_out = 0
-        while i < len(abbr):
+        j = 0
+        while i < len(abbr) and j < len(word):
             if abbr[i].isdigit():
-                number_list = []
-                number_list.append(abbr[i])
-                if int(number_list[0]) == 0:
+                if abbr[i] == '0':
                     return False
-                m = i
-                for j in range(m+1, len(abbr)):
-                    if abbr[j].isdigit():
-                        number_list.append(abbr[j])
-                        i += 1
-                    else:
-                        break
-                number_str = ''.join(number_list)
-                number_int = int(number_str)
-                Count_out = Count_out + number_int
-                word_abbr = word[i:i + number_int]
-                for j in range(0, len(number_str) - 1):  
-                    if number_str[j] == number_str[j + 1] and number_str[j] != '1':
-                        return False
-                i += 1
+                num = 0
+                while i < len(abbr) and abbr[i].isdigit():
+                    num = num*10 + int(abbr[i])
+                    i += 1
+                j += num
             else:
+                if abbr[i] != word[j]:
+                    return False
                 i += 1
-                Count_out += 1
-
-        if word[-1] =='d' and abbr[-1] == 'e':
-            return False
-
-        if word[-1] =='b' and abbr[-1] == 'c':
-            return False
-
-        
-        if Count_out != len(word):
-            return False
-        else:
-            return True
-
-
-                
-
-
-        """
-        :type word: str
-        :type abbr: str
-        :rtype: bool
-        """
-        
+                j += 1
+        return i==len(abbr) and j==len(word)
