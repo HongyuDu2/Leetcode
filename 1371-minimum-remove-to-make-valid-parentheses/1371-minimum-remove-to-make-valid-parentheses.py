1519a294d59da443133243b9e251666a31666d6f
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2025-11-06 14:28:49 -0800

    Time: 667 ms (5.06%), Space: 24 MB (6.87%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -2,11 +2,11 @@ class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         value = []
         local = []
-        for i, c in enumerate(s):
-            if c in "()":
-                value.append(c)
+        for i, num in enumerate(s):
+            if num in '()':
+                value.append(num)
                 local.append(i)
-
+        
         m = 0
         local_pop = []
         for i in range(len(value)):
@@ -17,7 +17,7 @@ class Solution:
             if m < 0:
                 local_pop.append(local[i])
                 m += 1
-
+        
         m = 0
         local_pop2 = []
         for i in range(len(value)-1, -1, -1):
@@ -28,9 +28,9 @@ class Solution:
             if m < 0:
                 local_pop2.append(local[i])
                 m += 1
-        
+
         local_final = sorted(local_pop + local_pop2)
         result = "".join(c for i, c in enumerate(s) if i not in local_final)
 
-        return result
+        return(result)
 
