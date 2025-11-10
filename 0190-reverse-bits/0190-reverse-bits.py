d0de18494cfc69c270c9a53ef7ced1cea1694076
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2025-11-10 10:46:55 -0800

    Time: 42 ms (37.4%), Space: 17.8 MB (42.84%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        m = 0
+        for _ in range(32):
+            m = m << 1
+            m += n & 1
+            n = n >> 1
+        return m
