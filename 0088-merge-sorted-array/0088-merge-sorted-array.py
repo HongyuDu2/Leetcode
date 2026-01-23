c007b707c6987fcc0be81a1b6f5338b2adc62d8c
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-01-23 14:43:25 -0800

    Time: 0 ms (100%), Space: 19.4 MB (13.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,25 +1,17 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        num1 = nums1[:m]
-        num2 = nums2[:n]
-        for i in range(0, len(num2)):
-            inserted = False
-            for j in range(0, len(num1)):
-                if num2[i] <= num1[j]:
-                    num1.insert(j, num2[i])
-                    inserted = True
-                    break
-            if not inserted:
-                num1.append(num2[i])
-            
-        for k in range(len(num1)):
-            nums1[k] = num1[k]
-    
-        return nums1
+        pos = m + n -1
+        m -= 1
+        n -= 1
+        while m >= 0 and n>= 0:
+            if nums1[m] > nums2[n]:
+                nums1[pos] = nums1[m]
+                m -= 1
+            else:
+                nums1[pos] = nums2[n]
+                n -= 1
+            pos -= 1
+        nums1[:n+1] = nums2[: n+1]
