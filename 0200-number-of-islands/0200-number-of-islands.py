6f512cd8643c3c3222f9899216f592cd668e5376
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-24 22:32:40 -0700

    Time: 243 ms (61.34%), Space: 21.6 MB (84.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,20 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        m, n = len(grid), len(grid[0])
-        
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        
-        count = 0
-        
-        def dfs(x, y):
-            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == "0":
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        nrow, ncol = len(grid), len(grid[0])
+        total_count = 0
+        def dfs (r, s):
+            if r < 0 or s < 0 or r >= nrow or s >= ncol or grid[r][s] == '0':
                 return
-            
-            grid[x][y] = '0'
-            
-            for dx, dy in directions:
-                dfs(x + dx, y + dy)
-                
-        for i in range(m):
-            for j in range(n):
+            grid[r][s] = '0'
+            dfs(r+1, s)
+            dfs(r-1, s)
+            dfs(r, s+1)
+            dfs(r, s-1)
+        for i in range(0, nrow):
+            for j in range(0, ncol):
                 if grid[i][j] == '1':
-                    count += 1
-                    dfs(i, j)
-                    
-        return count
+                    total_count += 1
+                dfs(i, j)
+        return total_count
