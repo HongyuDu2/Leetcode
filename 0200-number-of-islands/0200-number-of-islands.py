a27022859e622e95ee8a02432b0c3e2da8d57eb0
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-25 22:05:54 -0700

    Time: 247 ms (51.33%), Space: 21.7 MB (62.9%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,18 +3,19 @@ class Solution:
         if not grid:
             return 0
         nrow, ncol = len(grid), len(grid[0])
-        total_count = 0
-        def dfs (r, s):
-            if r < 0 or s < 0 or r >= nrow or s >= ncol or grid[r][s] == '0':
+        total = 0
+        def dfs(r, c):
+            if r<0 or c<0 or r >= nrow or c >= ncol or grid[r][c]=='0':
                 return
-            grid[r][s] = '0'
-            dfs(r+1, s)
-            dfs(r-1, s)
-            dfs(r, s+1)
-            dfs(r, s-1)
+            grid[r][c] = '0'
+            dfs(r-1, c)
+            dfs(r+1, c)
+            dfs(r, c-1)
+            dfs(r, c+1)
         for i in range(0, nrow):
             for j in range(0, ncol):
                 if grid[i][j] == '1':
-                    total_count += 1
+                    total += 1
                 dfs(i, j)
-        return total_count
+        return total
+
