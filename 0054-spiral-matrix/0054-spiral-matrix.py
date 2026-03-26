db8b88f54a32fc69022c184f6ce80f9efbfec2e2
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-25 23:11:34 -0700

    Time: 0 ms (100%), Space: 19.3 MB (59.7%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -6,9 +6,7 @@ class Solution:
         right = len(matrix[0])-1
         top = 0
         bottom = len(matrix)-1
-
         res = []
-
         while left <= right and top <= bottom:
             for i in range(left, right+1):
                 res.append(matrix[top][i])
@@ -22,9 +20,10 @@ class Solution:
                 for i in range(right, left-1, -1):
                     res.append(matrix[bottom][i])
                 bottom -= 1
+            
             if right >= left:
-
                 for i in range(bottom, top-1, -1):
                     res.append(matrix[i][left])
                 left += 1
         return res
+            
