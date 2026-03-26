01b41eedc3c695caa6b97352681d42472a79fcf6
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-25 22:26:54 -0700

    Time: 3389 ms (79.69%), Space: 19.3 MB (97.23%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,29 +1,21 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        R, C = len(board), len(board[0])
+        nrow, ncol = len(board), len(board[0])
 
         def dfs(r, c, k):
             if k == len(word):
                 return True
-            if r < 0 or r >= R or c < 0 or c >= C:
+            if r<0 or c<0 or r>=nrow or c>=ncol or board[r][c] != word[k]:
                 return False
-            if board[r][c] != word[k]:
-                return False
-            
             tmp = board[r][c]
-            board[r][c] = "#"
-            found = (
-                dfs(r-1, c, k+1) or
-                dfs(r+1, c, k+1) or
-                dfs(r, c+1, k+1) or
-                dfs(r, c-1, k+1)
-            )
-
+            board[r][c] = '#'
+            test = (dfs(r-1,c,k+1) or dfs(r+1,c,k+1) or dfs(r,c-1,k+1) or dfs(r,c+1,k+1))
             board[r][c] = tmp
-            return found
-
-        for i in range(R):
-            for j in range(C):
-                if board[i][j] == word[0] and dfs(i, j, 0):
+            return test
+        
+        for i in range(0, nrow):
+            for j in range(0, ncol):
+                if board[i][j] == word[0] and dfs(i,j,0):
                     return True
         return False
+                    
