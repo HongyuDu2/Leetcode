6fce902f0e7f3315a2d13dafb4954727ede9fb08
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-26 16:59:54 -0700

    Time: 21 ms (91.78%), Space: 22.2 MB (43.55%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,26 +2,26 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         if not heights or not heights[0]:
             return []
-        R, C = len(heights), len(heights[0])
-        dirs = ((0,-1), (0,1), (1,0), (-1,0))
-
+        nrow, ncol = len(heights), len(heights[0])
+        dirs = [(1,0), (0,1), (-1,0), (0,-1)]
+        
         def dfs(r, c, vis):
             vis[r][c] = True
-            for dr, dc in dirs:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < R and 0 <= nc < C and not vis[nr][nc] and heights[nr][nc] >= heights[r][c]:
+            for i, j in dirs:
+                nr, nc = r+i, c+j
+                if 0 <= nr < nrow and 0 <= nc < ncol and not vis[nr][nc] and heights[nr][nc] >= heights[r][c]:
                     dfs(nr, nc, vis)
         
-        pac = [[False]*C for _ in range(R)]
-        alt = [[False]*C for _ in range(R)]
+        pac = [[False]*ncol for _ in range(nrow)]
+        atc = [[False]*ncol for _ in range(nrow)]
 
-        for c in range(C):
-            dfs(0, c, pac)
-        for r in range(R):
-            dfs(r, 0, pac)
-        for c in range(C):
-            dfs(R-1, c, alt)
-        for r in range(R):
-            dfs(r, C-1, alt)
+        for k in range(ncol):
+            dfs(0, k, pac)
+        for k in range(nrow):
+            dfs(k, 0, pac)
+        for k in range(ncol):
+            dfs(nrow-1, k, atc)
+        for k in range(nrow):
+            dfs(k, ncol-1, atc)
         
-        return [[r,c] for r in range(R) for c in range(C) if pac[r][c] and alt[r][c]]
+        return [[r, s] for r in range(nrow) for s in range(ncol) if pac[r][s] and atc[r][s]]
