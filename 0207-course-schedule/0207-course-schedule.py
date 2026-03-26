a0fdb2e6b8647e5e5b5e2fbb55fba6a7cfe4edd9
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-26 16:09:58 -0700

    Time: 7 ms (46.14%), Space: 20.5 MB (61.83%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,34 +1,20 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        graph = {i : [] for i in range(numCourses)}
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        course_arr = [[] for _ in range(numCourses)]
+        degree = [0]*numCourses
+
+        for cur, pre in prerequisites:
+            course_arr[pre].append(cur)
+            degree[cur] += 1
         
-        for course, prereq in prerequisites:
-            graph[course].append(prereq)
-            
-        visited = [0]*numCourses
-        
-        def dfs(course):
-            if visited[course] == 1:
-                return False
-            if visited[course] == 2:
-                return True
-            
-            visited[course] = 1
-            
-            for prereq in graph[course]:
-                if not dfs(prereq):
-                    return False
-                
-            visited[course] = 2
-            return True
-        
-        for course in range(numCourses):
-            if visited[course] == 0:
-                if not dfs(course):
-                    return False
-        return True
+        queue = deque([i for i in range(numCourses) if degree[i] == 0])
+
+        Count = 0
+        while queue:
+            curr = queue.popleft()
+            Count += 1
+            for nums in course_arr[curr]:
+                degree[nums] -= 1
+                if degree[nums] == 0:
+                    queue.append(nums)
+        return Count == numCourses
