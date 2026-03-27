1c60d439fb9885c7e02d2e25bf11267a4f4ae4c6
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-27 15:50:45 -0700

    Time: 86 ms (6.57%), Space: 28.6 MB (58.7%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,10 +4,12 @@ class Solution:
         s1, s2 = 0, 0
         while s2 < len(prices)-1:
             s2 += 1
-            if prices[s1] > prices[s2]:
+            if prices[s2] >= prices[s1]:
+                profit = max(profit, prices[s2]-prices[s1])
+            else:
                 s1 = s2
-            profit = max(profit, prices[s2]-prices[s1])
         return profit
+
 
 
 
