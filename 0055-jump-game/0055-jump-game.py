61c885dcb1c36c9667685fb0f232782bd9051192
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-27 17:23:11 -0700

    Time: 16 ms (70.64%), Space: 20.3 MB (57.6%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,9 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         max_distance = 0
-        for i in range(0, len(nums)):
+        for i in range(len(nums)):
             if i > max_distance:
                 return False
-            max_distance = max(max_distance, i + nums[i])
-            if max_distance >= len(nums) - 1:
+            max_distance = max(max_distance, i+nums[i])
+            if max_distance >= len(nums)-1:
                 return True
