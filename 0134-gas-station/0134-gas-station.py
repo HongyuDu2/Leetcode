79e0b50496a3af58b766fdf64ddc76a120908160
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-27 21:15:08 -0700

    Time: 31 ms (40.42%), Space: 25.8 MB (99.33%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,38 @@
+'''
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        begin = -1
+        for i in range(len(gas)):
+            if gas[i] >= cost[i]:
+                begin = i
+                break
+        
+        if begin == -1:
+            return -1
+
+         # 初始化油量
+        tank = 0
+        n = len(gas)
+
+        # 尝试从找到的起点验证是否可以完成一圈
+        for i in range(n):
+            curr = (begin + i) % n  # 环形路径索引
+            tank += gas[curr] - cost[curr]  # 更新当前油量
+
+            if tank < 0:  # 如果油量不足，返回 -1
+                return -1
+        
+        return begin
+'''
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total, curr = 0, 0
+        start = 0
+        for i in range(0, len(gas)):
+            total += gas[i] - cost[i]
+            curr += gas[i]- cost[i]
+            if curr < 0:
+                start = i + 1
+                curr = 0
+        return start if total >= 0 else -1
