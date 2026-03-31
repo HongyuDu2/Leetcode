b2e415f3c5e944b801029d21e51eb83f9b276a7f
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-03-31 14:57:25 -0700

    Time: 99 ms (27.18%), Space: 31.1 MB (41.4%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -5,17 +5,23 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pivot_val = random.choice(nums)
-        large, medium, small = [], [], []
+        # pivot_val = random.choice(nums)
+        # large, medium, small = [], [], []
+        # for num in nums:
+        #     if num > pivot_val:
+        #         large.append(num)
+        #     elif num < pivot_val:
+        #         small.append(num)
+        #     else:
+        #         medium.append(num)
+        # if k <= len(large):
+        #     return self.findKthLargest(large, k)
+        # if k > len(large) + len(medium):
+        #     return self.findKthLargest(small, k-len(large)-len(medium))
+        # return pivot_val
+        heap = []
         for num in nums:
-            if num > pivot_val:
-                large.append(num)
-            elif num < pivot_val:
-                small.append(num)
-            else:
-                medium.append(num)
-        if k <= len(large):
-            return self.findKthLargest(large, k)
-        if k > len(large) + len(medium):
-            return self.findKthLargest(small, k-len(large)-len(medium))
-        return pivot_val
+            heapq.heappush(heap, num)
+        for i in range(1, len(nums)-k+1):
+            heapq.heappop(heap)
+        return heap[0]
