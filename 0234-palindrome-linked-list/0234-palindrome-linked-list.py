2937c507f0db25e4974d112f1371bc5694f06bce
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-01 16:32:29 -0700

    Time: 35 ms (57.31%), Space: 42.3 MB (94.58%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -11,7 +11,8 @@ class Solution:
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        pre, cur = None, slow
+        pre = None
+        cur = slow
         while cur:
             nxt = cur.next
             cur.next = pre
@@ -19,10 +20,10 @@ class Solution:
             cur = nxt
         second = pre
         p1, p2 = head, second
-        ok = True
         while p2:
             if p1.val != p2.val:
                 return False
             p1 = p1.next
             p2 = p2.next
         return True
+        
