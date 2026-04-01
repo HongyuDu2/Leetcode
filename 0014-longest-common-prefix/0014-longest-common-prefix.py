8e4a7637c7c432022500c490f03c82297f1d24a6
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-01 15:40:22 -0700

    Time: 3 ms (21.87%), Space: 19.3 MB (83.85%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,30 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # overlap = strs[0]
-        # for i in range(len(strs)):
-        #     if len(overlap) <= len(strs[i]) and strs[i].startswith(overlap):
-        #         continue
-        #     else:
-        #         overlap_new = []
-        #         l=0
-        #         p=0
-        #         for j in range(len(strs[i])):
-        #             for k in range(len(overlap)):
-        #                 if strs[i][j] == overlap[k+p]:
-        #                     overlap_new.append(strs[i][j])
-        #                     p += 1
-        #                     break
-        #                 else:
-        #                     l = 1
-        #                     break
-        #             if l == 1:
-        #                 break
-        #         overlap = overlap_new
-        #         overlap = ''.join(overlap)
-                
-        # return overlap
-        if not strs:
-            return ""
+        # if not strs:
+        #     return ""
         for i in range(len(strs[0])):
             character = strs[0][i]
             for j in range(1, len(strs)):
