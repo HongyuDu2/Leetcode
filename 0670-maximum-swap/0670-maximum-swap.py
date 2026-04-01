8fe62bba55a34e85027d32228a2fcfd82c355e1f
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-01 14:44:21 -0700

    Time: 0 ms (100%), Space: 19.4 MB (51.93%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,10 +1,10 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
         A = list(str(num))
-        dist = {int(x) : i for i, x in enumerate(A)}
+        max_pos = {int(x):i for i, x in enumerate(A)}
         for i, x in enumerate(A):
             for j in range(9, int(x), -1):
-                if dist.get(j, -1) > i:
-                    A[i], A[dist[j]] = A[dist[j]], A[i]
+                if max_pos.get(j,-1) > i:
+                    A[i], A[max_pos[j]] = A[max_pos[j]], A[i]
                     return int("".join(A))
         return num
