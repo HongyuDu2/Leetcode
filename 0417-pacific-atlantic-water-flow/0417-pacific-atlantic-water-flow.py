b0b35c99be14b5f9a811d34ab19bd6ba3575289a
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-01 16:03:24 -0700

    Time: 35 ms (38.82%), Space: 22.1 MB (51.87%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -3,25 +3,25 @@ class Solution:
         # if not heights or not heights[0]:
         #     return []
         nrow, ncol = len(heights), len(heights[0])
-        dirs = [(1,0), (0,1), (-1,0), (0,-1)]
-        
-        def dfs(r, c, vis):
-            vis[r][c] = True
-            for i, j in dirs:
-                nr, nc = r+i, c+j
-                if 0 <= nr < nrow and 0 <= nc < ncol and not vis[nr][nc] and heights[nr][nc] >= heights[r][c]:
-                    dfs(nr, nc, vis)
+        dir = ([1,0],[-1,0],[0,1],[0,-1])
+        def dfs(n, r, vis):
+            vis[n][r] = True
+            for i, j in dir:
+                dn, dr = n+i, r+j
+                if 0<=dn<nrow and 0<=dr<ncol and not vis[dn][dr] and heights[dn][dr]>=heights[n][r]:
+                    dfs(dn,dr,vis)
         
         pac = [[False]*ncol for _ in range(nrow)]
         atc = [[False]*ncol for _ in range(nrow)]
 
-        for k in range(ncol):
-            dfs(0, k, pac)
-        for k in range(nrow):
-            dfs(k, 0, pac)
-        for k in range(ncol):
-            dfs(nrow-1, k, atc)
-        for k in range(nrow):
-            dfs(k, ncol-1, atc)
+        for i in range(nrow):
+            dfs(i,0,pac)
+        for i in range(ncol):
+            dfs(0,i,pac)
+        for i in range(nrow):
+            dfs(i,ncol-1,atc)
+        for i in range(ncol):
+            dfs(nrow-1,i,atc)
         
-        return [[r, s] for r in range(nrow) for s in range(ncol) if pac[r][s] and atc[r][s]]
+        return [[r,s] for r in range(nrow) for s in range(ncol) if pac[r][s] == atc[r][s] == True]
+
