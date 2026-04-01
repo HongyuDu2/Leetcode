4e44d022f4e03d480303f786de49428918e3b8c5
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-01 11:51:43 -0700

    Time: 3530 ms (65.75%), Space: 19.2 MB (96.76%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,20 +1,18 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         nrow, ncol = len(board), len(board[0])
-
-        def dfs(r, c, k):
+        def dfs(i, j, k):
             if k == len(word):
                 return True
-            if r<0 or c<0 or r>=nrow or c>=ncol or board[r][c] != word[k]:
+            if i<0 or j<0 or i>=nrow or j>=ncol or board[i][j]!=word[k]:
                 return False
-            tmp = board[r][c]
-            board[r][c] = '#'
-            test = (dfs(r-1,c,k+1) or dfs(r+1,c,k+1) or dfs(r,c-1,k+1) or dfs(r,c+1,k+1))
-            board[r][c] = tmp
+            tmp=board[i][j]
+            board[i][j] = "#"
+            test = (dfs(i+1,j,k+1) or dfs(i,j+1,k+1) or dfs(i-1,j,k+1) or dfs(i,j-1,k+1))
+            board[i][j]=tmp
             return test
-        
-        for i in range(0, nrow):
-            for j in range(0, ncol):
+        for i in range(nrow):
+            for j in range(ncol):
                 if board[i][j] == word[0] and dfs(i,j,0):
                     return True
         return False
