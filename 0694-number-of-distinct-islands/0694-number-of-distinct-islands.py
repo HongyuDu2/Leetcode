5c08197c59e5fdbf6d415a85c7e107b722080579
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-01 12:03:43 -0700

    Time: 28 ms (84.65%), Space: 20.5 MB (67.16%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -1,21 +1,20 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
         nrow, ncol = len(grid), len(grid[0])
-        def dfs(r, c, direction):
-            if r<0 or c<0 or r>=nrow or c >= ncol or grid[r][c]==0:
+        def dfs(i,j,direction):
+            if i<0 or j<0 or i>=nrow or j>=ncol or grid[i][j] ==0:
                 return ""
-            grid[r][c] = 0
+            grid[i][j] = 0
             path = direction
-            path += dfs(r-1, c, "D")
-            path += dfs(r+1, c, "U")
-            path += dfs(r, c+1, "L")
-            path += dfs(r, c-1, "R")
+            path += dfs(i+1,j,'D')
+            path += dfs(i-1,j,'U')
+            path += dfs(i,j+1,'L')
+            path += dfs(i,j-1,'R')
             path += 'B'
             return path
         shape = set()
         for i in range(nrow):
             for j in range(ncol):
                 if grid[i][j] == 1:
-                    path = dfs(i,j,'S')
-                    shape.add(path)
+                    shape.add(dfs(i,j,'S'))
         return len(shape)
