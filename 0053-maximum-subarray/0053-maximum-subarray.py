5d94190cbf0e7b46bd7b36241a05a394339f6523
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-02 16:38:21 -0700

    Time: 40 ms (41.56%), Space: 31.6 MB (13.67%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,13 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         current_sum = max_sum = nums[0]
-    
         for num in nums[1:]:
-        # 决定是“加入”还是“重启”
-        # 如果 current_sum 是负数，max(num, negative + num) 会直接选择 num
-            current_sum = max(num, current_sum + num)
-        
-        # 更新全局最大值
+            current_sum = max(num, num+current_sum)
             max_sum = max(max_sum, current_sum)
-        
         return max_sum
