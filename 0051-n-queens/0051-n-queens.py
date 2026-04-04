215937f0d3f9c6a6e4ceb0bbd8d81dee919eaf24
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-04 13:27:33 -0700

    Time: 11 ms (68.37%), Space: 19.9 MB (6.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -4,29 +4,26 @@ class Solution:
         board = [["."]*n for _ in range(n)]
 
         cols = set()
-        pos_diag = set()
-        neg_diag = set()
+        left_diag = set()
+        right_diag = set()
 
         def backtrack(r):
             if r == n:
                 res.append(["".join(row) for row in board])
                 return
-            
             for c in range(n):
-                if c in cols or (c + r) in pos_diag or (r-c) in neg_diag:
+                if c in cols or (c+r) in left_diag or (r-c) in right_diag:
                     continue
-                
                 cols.add(c)
-                pos_diag.add(r+c)
-                neg_diag.add(r-c)
+                left_diag.add(c+r)
+                right_diag.add(r-c)
                 board[r][c] = "Q"
 
-                backtrack(r + 1)
+                backtrack(r+1)
 
                 cols.remove(c)
-                pos_diag.remove(r+c)
-                neg_diag.remove(r-c)
+                left_diag.remove(r+c)
+                right_diag.remove(r-c)
                 board[r][c] = "."
-        
-        backtrack (0)
+        backtrack(0)
         return res
