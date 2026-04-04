9ed72f414b03a4306c81ee435a9bd3c6dc4c03a7
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-04 14:41:35 -0700

    Time: 0 ms (100%), Space: 20.4 MB (71.73%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,19 +1,19 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         course_arr = [[] for _ in range(numCourses)]
-        degree = [0]* numCourses
+        degree = [0]*numCourses
 
-        for curr, pre in prerequisites:
-            course_arr[pre].append(curr)
-            degree[curr] += 1
+        for pre, cur in prerequisites:
+            course_arr[pre].append(cur)
+            degree[cur] += 1
         
         queue = deque([i for i in range(numCourses) if degree[i] == 0])
-
         Count = 0
+
         while queue:
-            curr = queue.popleft()
+            cur = queue.popleft()
             Count += 1
-            for nums in course_arr[curr]:
+            for nums in course_arr[cur]:
                 degree[nums] -= 1
                 if degree[nums] == 0:
                     queue.append(nums)
