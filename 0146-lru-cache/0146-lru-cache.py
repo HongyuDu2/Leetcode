4bf8b6963bd382001994aae32ba4b3ef070ddd30
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-04-04 17:38:59 -0700

    Time: 99 ms (86.94%), Space: 78.7 MB (19.07%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,71 +1,69 @@
 class Node:
-    """A node in the doubly linked list."""
-    def __init__(self, key, value):
+    def __init__(self, key=0, value=0):
         self.key = key
         self.value = value
         self.prev = None
         self.next = None
 
 class LRUCache:
-    def __init__(self, capacity):
-        """
-        Initialize the LRU cache with a given capacity.
-        """
+    def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}  # HashMap to store key -> node
-        # Initialize a dummy head and tail for the doubly linked list
-        self.head = Node(0, 0)  
-        self.tail = Node(0, 0)
+        self.cache = {}
+        # 哨兵节点，简化边界处理
+        self.head = Node()
+        self.tail = Node()
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def _remove(self, node):
-        """
-        Remove a node from the doubly linked list.
-        """
-        node.prev.next = node.next
-        node.next.prev = node.prev
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._move_to_head(node)
+        return node.value
 
-    def _add_to_head(self, node):
-        """
-        Add a node right after the dummy head.
-        """
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._move_to_head(node)
+        else:
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_node(new_node)
+            if len(self.cache) > self.capacity:
+                removed = self._pop_tail()
+                if removed:
+                    del self.cache[removed.key]
+
+    # --- 以下是你代码中引用但未定义的“发动机”部分 ---
+
+    def _add_node(self, node):
+        """将节点插入到 head 后面（最新使用）"""
         node.prev = self.head
         node.next = self.head.next
         self.head.next.prev = node
         self.head.next = node
 
-    def get(self, key):
-        """
-        Return the value of the key if it exists, otherwise return -1.
-        """
-        if key in self.cache:
-            node = self.cache[key]
-            # Move the node to the head (most recently used)
-            self._remove(node)
-            self._add_to_head(node)
-            return node.value
-        return -1
+    def _remove_node(self, node):
+        """将节点从链表中彻底断开"""
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
 
-    def put(self, key, value):
-        """
-        Add or update the value of the key.
-        If the cache exceeds capacity, evict the least recently used key.
-        """
-        if key in self.cache:
-            # Remove the old node
-            self._remove(self.cache[key])
-        # Create a new node
-        new_node = Node(key, value)
-        self.cache[key] = new_node
-        self._add_to_head(new_node)
+    def _move_to_head(self, node):
+        """移动到开头：先删后加"""
+        self._remove_node(node)
+        self._add_node(node)
 
-        if len(self.cache) > self.capacity:
-            # Remove the least recently used node (from the tail)
-            lru = self.tail.prev
-            self._remove(lru)
-            del self.cache[lru.key]
-        
+    def _pop_tail(self):
+        """弹出最久未使用的节点（tail 之前那个）"""
+        res = self.tail.prev
+        if res == self.head: # 链表为空的情况（虽然 capacity>0 不会发生）
+            return None
+        self._remove_node(res)
+        return res    
 
 
 # Your LRUCache object will be instantiated and called as such:
