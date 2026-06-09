95627692e8a77418b5d5fdcb59a3e28897ad9da5
HONGYU DU <145506535+HongyuDu2@users.noreply.github.com>
2026-06-09 10:09:56 -0700

    Time: 0 ms (100%), Space: 19.3 MB (24.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,15 @@
 class Solution:
     def rob(self, nums) -> int:
-        dp1 = 0
-        dp2 = 0
-        for i in range(0, len(nums)):
-            cur = max(dp1, dp2 + nums[i])
-            dp2 = dp1
-            dp1 = cur
-        return cur
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        elif n == 2:
+            return max(nums[0], nums[1])
+        else:
+            dp1 = nums[0]
+            dp2 = max(nums[0], nums[1])
+            for i in range(2, n):
+                curr = max(dp1 + nums[i], dp2)
+                dp1 = dp2
+                dp2 = curr
+        return curr
